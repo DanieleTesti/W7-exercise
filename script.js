5bbbf7940868413f4bb4e28f2aa1957a7731445c
DanieleTesti <danieletesti14@gmail.com>
2023-01-29 19:10:22 +0100

    last update

--- script.js
+++ script.js
@@ -14,14 +14,17 @@ const fetchByQuery = async (query) => {
 const prendiCanzone = async function () {
   try {
     const res = await fetchByQuery("vasco rossi");
-    for (let i = 0; i < 4; i++) {
+    //a ogni refrash cambia le canzoni della prima sezione, in modo da poter vedere le varie views
+    let num = Math.floor(Math.random() * 20);
+    let random = num + 4;
+    for (let i = num; i < random; i++) {
       let song = res[i];
       contenitorecard.innerHTML += `
       <div class='col'>
-        <div class="card mt-5 bg-warning m-1 " style="width: 18rem; height:40rem">
+        <div class="forma-carta card mt-5 bg-warning m-1 " style="width: 18rem; height:40rem">
           <img class="card-img-top mt-3 img-fluid w-100" style="height:420px" src="${song.album.cover_medium}" alt="Card image cap">
           <div class="card-body d-flex flex-column align-items-center justify-content-between  ">
-            <h5 class="card-title">${song.title} </h5>
+            <h5 class="card-title"songRank="${song.rank}">${song.title} </h5>
             <p class="card-text">Artist: ${song.artist.name} </p>
             <a id="skip" href="${song.link}" class="btn btn-dark">Prewiew</a>
           </div>
@@ -45,16 +48,16 @@ async function favouriteSong() {
   const song = data.data.find((song) => song.title === "Senza Parole");
   favSong.innerHTML = `
   <div class='col'>
-  <div class="card mt-5 bg-warning m-1 " style="width: 18rem; height:40rem">
-  <img class="card-img-top mt-3 img-fluid w-100" style="height:420px" src="${song.album.cover_medium}" alt="Card image cap">
-  <div class="card-body d-flex flex-column align-items-center justify-content-between  ">
-  <h5 class="card-title">${song.title} </h5>
-  <p class="card-text">Artist: ${song.artist.name} </p>
-  <a id="skip" href="${song.link}" class="btn btn-dark">Prewiew</a>
+  <div class="forma-carta card mt-5 bg-warning m-1 " style="width: 18rem; height:40rem">
+    <img class="card-img-top mt-3 img-fluid w-100" style="height:420px" src="${song.album.cover_medium}" alt="Card image cap">
+    <div class="card-body d-flex flex-column align-items-center justify-content-between  ">
+      <h5 class="card-title"songRank="${song.rank}">${song.title} </h5>
+      <p class="card-text">Artist: ${song.artist.name} </p>
+      <a id="skip" href="${song.link}" class="btn btn-dark">Prewiew</a>
+    </div>
   </div>
-  </div>
-  </div>
-  `;
+</div>
+`;
 }
 
 favouriteSong();
@@ -85,19 +88,48 @@ async function favouriteAlbum() {
 }
 favouriteAlbum();
 
-const ordinazione = async function () {
-  let titoli = prendiCanzone();
-  let sorted = titoli.sort((a, b) => {
-    return a.rank - b.rank;
+//bottoni
+
+const arrayDiTitoli = () => {
+  let h5 = document.querySelectorAll("h5");
+  let titoli = [];
+  h5.forEach((titolo) => {
+    titoli.push({
+      titolo: titolo.innerText,
+      rank: Number(titolo.getAttribute("songRank")),
+    });
   });
+  return titoli;
+};
+
+const titoliInOrdine = () => {
+  let titoli = arrayDiTitoli();
+  let sorted = titoli.map((song) => song.titolo).sort();
   console.log(sorted);
-  let alert = document.querySelector("canzoniOrdinate");
+  let alert = document.querySelector(".modal ul.canzoniOrdinate");
   alert.innerHTML = "";
-  sorted.forEach((canzone) => {
+  sorted.forEach((song) => {
     alert.innerHTML += `<li class='list-group-item'>
-      ${canzone.titolo} - ${canzone.rank}
+      ${song}
+      
       </li>`;
   });
 };
 
-ordinazione();
+const soloTitolo = () => {
+  let titoli = arrayDiTitoli();
+  let sorted = titoli.sort((a, b) => {
+    return a.rank - b.rank;
+  });
+  console.log(sorted);
+  let alert = document.querySelector(".alert ul.canzoniOrdinate");
+  alert.innerHTML = "";
+  sorted.forEach((song) => {
+    alert.innerHTML += `<li class='list-group-item'>
+      ${song.titolo} - ${song.rank}
+      </li>`;
+  });
+  alert.parentElement.classList.toggle("d-none");
+};
+
+titoliInOrdine();
